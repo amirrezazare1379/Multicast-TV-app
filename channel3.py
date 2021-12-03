9c17344393acacfd27ff0c966d82edad79df0559
Amirreza Zare <88764814+amirrezazare1379@users.noreply.github.com>
2021-12-03 20:35:27 +0330

    Update channel3.py

--- channel3.py
+++ channel3.py
@@ -1,18 +1,53 @@
-#tcp connection for reciev the playlist off channels
+# tcp connection for reciev the playlist off channels
 import socket
 import messages
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # IPv4 , # tcp
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # IPv4 , # tcp
+HOST = "192.168.1.6"
+PORT = 8989
+clientSocket.connect((HOST, PORT))
+requestMessagge = messages.requestPlaylistChannel3
+clientSocket.sendall(requestMessagge.encode('ascii'))
 
-s.connect(("192.168.1.6", 8989))
-sendMsg1 = messages.requestPlaylistChannel3
-s.sendall(sendMsg1.encode('ascii'))
+# reciev playlist
+rcvPlaylistMessage = clientSocket.recv(1024)
+print(rcvPlaylistMessage.decode('ascii'))
 
-revcMsg = s.recv(1024)
-print(revcMsg.decode('ascii'))
+# recv msg that channel is playing ro not
+channelStatus = clientSocket.recv(1024)
+channelStatus = channelStatus.decode('ascii')
+if channelStatus == messages.channel3IsNotPlaying:
+    print(channelStatus)
+elif channelStatus == messages.channel3IsPlaying:
+    print(channelStatus)
+    ackOrNack = input(messages.wantToWatch)
+    clientSocket.sendall(ackOrNack.encode('ascii'))
+
+clientSocket.close()
 
 
-revcMsg = s.recv(1024)
-print(revcMsg.decode('ascii'))
 
-s.close()
+
+#UDP
+
+
+# RECEIVER
+
+print("start udp ...")
+import socket
+import struct
+
+MCAST_GRP = '224.1.1.1'
+MCAST_PORT = 5004
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+sock.bind(('', MCAST_PORT))
+mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+while True:
+    print(sock.recv(10240))
+
