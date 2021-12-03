27ce179bf16688184b134dd4d1098ec02c7f0c9e
Amirreza Zare <88764814+amirrezazare1379@users.noreply.github.com>
2021-12-03 20:36:40 +0330

    Update messages.py

--- messages.py
+++ messages.py
@@ -1,3 +1,15 @@
 requestPlaylistChannel1="hi server, i'm channel 1"
 requestPlaylistChannel2="hi server, i'm channel 2"
 requestPlaylistChannel3="hi server, i'm channel 3"
+
+channel1IsPlaying = "channel 1 is playing..."
+channel1IsNotPlaying = "channel 1 is not playing!"
+channel2IsPlaying = "channel 2 is playing..."
+channel2IsNotPlaying = "channel 2 is not playing!"
+channel3IsPlaying = "channel 3 is playing..."
+channel3IsNotPlaying = "channel 3 is not playing!"
+
+YES = "yes"
+NO = "no"
+
+wantToWatch = "do you want to watch? "
