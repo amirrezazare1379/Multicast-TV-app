3e801cbfd1a9b96492e544a2809cec51f500d23d
Amirreza Zare <88764814+amirrezazare1379@users.noreply.github.com>
2021-12-03 20:36:13 +0330

    Update mainserver.py

--- mainserver.py
+++ mainserver.py
@@ -1,9 +1,10 @@
+import time
+
 import messages
 import socket
 import datetime
 
 # set the channels playlist:
-
 AllChannels_playList = {
     "channel1": {
         "fast and furious 10": [0, 1, 2, 3],
@@ -19,80 +20,217 @@ AllChannels_playList = {
     }
 }
 
-### tcp connection to channels for send playlist of each channel and :
+# tcp connection to channels for send playlist of each channel and : #
 
 # create tcp socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # IPv4 , # tcp
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # IPv4 , # tcp
 
 # create host and port
-s.bind(("192.168.1.6", 8989))
+HOST = "192.168.1.6"
+PORT = 8989
+serverSocket.bind((HOST, PORT))
 
 # server is waiting for a channel request
-s.listen(1)
-
+serverSocket.listen(1)
 print("Run Server... \n")
 
-c, addr = s.accept()
+client, address = serverSocket.accept()
 
-print("connect to %s " % str(addr))
+print("connected to %s " % str(address))
 
 # recieve channel request
-cmsg = c.recv(1024).decode('ascii')
+clientRequestMessage = client.recv(1024).decode('ascii')
 
-# check that which channel sends request for recieve playlist
-if cmsg == messages.requestPlaylistChannel1:
-    c.sendall(str(AllChannels_playList["channel1"]).encode('ascii'))
+# send playlist
+if clientRequestMessage == messages.requestPlaylistChannel1:
+    client.sendall(str(AllChannels_playList["channel1"]).encode('ascii'))
 
-if cmsg == messages.requestPlaylistChannel2:
-    c.sendall(str(AllChannels_playList["channel2"]).encode('ascii'))
+if clientRequestMessage == messages.requestPlaylistChannel2:
+    client.sendall(str(AllChannels_playList["channel2"]).encode('ascii'))
 
-if cmsg == messages.requestPlaylistChannel3:
-    c.sendall(str(AllChannels_playList["channel3"]).encode('ascii'))
+if clientRequestMessage == messages.requestPlaylistChannel3:
+    client.sendall(str(AllChannels_playList["channel3"]).encode('ascii'))
 
 # check time of channel request
-time = datetime.datetime.now().hour
+channelRequestTime = datetime.datetime.now().hour
 
 # check the channels are playing :
-if cmsg == messages.requestPlaylistChannel1:
-    if time not in AllChannels_playList["channel1"]["fast and furious 10"] and time not in \
+if clientRequestMessage == messages.requestPlaylistChannel1:
+    if channelRequestTime not in AllChannels_playList["channel1"]["fast and furious 10"] and channelRequestTime not in \
             AllChannels_playList["channel1"]["spyderman"]:
-        errMsg = "channel1 is not playing !!!"
-        c.sendall(errMsg.encode('ascii'))
+        client.sendall(messages.channel1IsNotPlaying.encode('ascii'))
+    else:
+        client.sendall(messages.channel1IsPlaying.encode('ascii'))
 
-if cmsg == messages.requestPlaylistChannel2:
-    if time not in AllChannels_playList["channel2"]["venom"] and time not in AllChannels_playList["channel2"][
-        "spyderman"]:
-        errMsg = "channel2 is not playing !!!"
-        c.sendall(errMsg.encode('ascii'))
+if clientRequestMessage == messages.requestPlaylistChannel2:
+    if channelRequestTime not in AllChannels_playList["channel2"]["venom"] and channelRequestTime not in \
+            AllChannels_playList["channel2"]["spyderman"]:
+        client.sendall(messages.channel2IsNotPlaying.encode('ascii'))
+    else:
+        client.sendall(messages.channel2IsPlaying.encode('ascii'))
 
-if cmsg == messages.requestPlaylistChannel3:
-    if time not in AllChannels_playList["channel3"]["fast and furious 10"] and time not in \
+if clientRequestMessage == messages.requestPlaylistChannel3:
+    if channelRequestTime not in AllChannels_playList["channel3"]["fast and furious 10"] and channelRequestTime not in \
             AllChannels_playList["channel3"]["venom"]:
-        errMsg = "channel3 is not playing !!!"
-        c.sendall(errMsg.encode('ascii'))
+        client.sendall(messages.channel3IsNotPlaying.encode('ascii'))
+    else:
+        client.sendall(messages.channel3IsPlaying.encode('ascii'))
 
-c.close()
+requestWatchFlagChannel1 = False
+requestWatchFlagChannel2 = False
+requestWatchFlagChannel3 = False
 
-# =======================================================================================================
-# =======================================================================================================
+import json
+
+# recv ack or nack for watching
+ackOrNack = client.recv(1024).decode('ascii')
+if ackOrNack.lower() == messages.NO:
+    print('tcp connection closed.')
+    client.close()
+
+# udp setting for channel 1 :
+if ackOrNack.lower() == messages.YES and clientRequestMessage == messages.requestPlaylistChannel1:
+    requestWatchFlagChannel1 = True
+    print(" sending image ...")
+    # client.close()
+    print("strar udp connection ...")
+    time.sleep(3)
+    # SENDER
+    import socket
+
+    group = '224.1.1.1'
+    port = 5004
+    # 2-hop restriction in network
+    ttl = 1
+    sock = socket.socket(socket.AF_INET,
+                         socket.SOCK_DGRAM,
+                         socket.IPPROTO_UDP)
+    sock.setsockopt(socket.IPPROTO_IP,
+                    socket.IP_MULTICAST_TTL,
+                    ttl)
+    sock.sendto(b'hello udp client', (group, port))
+
+    # if channelRequestTime in AllChannels_playList["channel1"]["fast and furious 10"]:
+    #     for imgNumber in range(0, 100, 5):
+    #         f = open(f"./save/fast/{imgNumber}.png", "r")
+    #         f = f.read()
+    #         sock.sendto(str(f), (group, port))
+    # elif channelRequestTime in AllChannels_playList["channel1"]["spyderman"]:
+    #     for imgNumber in range(0, 100, 5):
+    #         f = open(f"./save/spyderman/{imgNumber}.png", "r")
+    #         f = f.read()
+    #         sock.sendto(str(f), (group, port))
+
+# ======================================================
 
 
-### Multicast UDP send image
+# udp setting for channel 2 :
+if ackOrNack.lower() == messages.YES and clientRequestMessage == messages.requestPlaylistChannel2:
+    requestWatchFlagChannel2 = True
+    print(" sending image ...")
+    # client.close()
+    print("strar udp connection ...")
+    time.sleep(3)
+    # SENDER
+    import socket
 
-# SENDER
+    group = '224.1.1.1'
+    port = 5004
+    # 2-hop restriction in network
+    ttl = 1
 
-import socket
-group = '224.1.1.1'
-port = 5004
+    sock = socket.socket(socket.AF_INET,
+                         socket.SOCK_DGRAM,
+                         socket.IPPROTO_UDP)
+    sock.setsockopt(socket.IPPROTO_IP,
+                    socket.IP_MULTICAST_TTL,
+                    ttl)
 
-# 2-hop restriction in network
-ttl = 2
+    sock.sendto(b'hello udp client', (group, port))
 
-sock = socket.socket(socket.AF_INET,
-                     socket.SOCK_DGRAM,
-                     socket.IPPROTO_UDP)
-sock.setsockopt(socket.IPPROTO_IP,
-                socket.IP_MULTICAST_TTL,
-                ttl)
+    # if channelRequestTime in AllChannels_playList["channel2"]["venom"]:
+    #     with open('./save/venom/0.png', 'rb') as f:
+    #         client.send(f.read())
+    #         l = f.read()
+    #         # im = Image.open(l)
+    #         # im.show()
+    #         f.close()
 
-sock.sendto(b"hello world", (group, port))
+    # import json
+    # import base64
+    # data = {}
+    # with open('./save/venom/0.png', mode='rb') as file:
+    #     img = file.read()
+    # data['img'] = base64.b64encode(img)
+    # sock.sendto(json.dumps(data), (group, port))
+    # import base64
+    # import json
+    # import cv2
+    #
+    # for i in range(0,100,5):
+    #     img = cv2.imread(f'./save/fast/{i}.png')
+    #     string = base64.b64encode(cv2.imencode('.png', img)[1]).decode()
+    #     dict = {
+    #         'img': string
+    #     }
+    #     with open(f'./jsonImage/{i}.json', 'w') as outfile:
+    #         json.dump(dict, outfile, ensure_ascii=False, indent=4)
+
+    #
+    # elif channelRequestTime in AllChannels_playList["channel2"]["spyderman"]:
+    #     import json
+    #     import base64
+    #     data = {}
+    #     with open('./save/spyderman/0.png', mode='rb') as file:
+    #         img = file.read()
+    #     data['img'] = base64.b64encode(img)
+    #     sock.sendto(json.dumps(data), (group, port))
+
+# ============================================================================
+
+import cv2
+
+# udp setting for channel 3 :
+if ackOrNack.lower() == messages.YES and clientRequestMessage == messages.requestPlaylistChannel3:
+    requestWatchFlagChannel3 = True
+    print(" sending image ...")
+    # client.close()
+    print("strar udp connection ...")
+    time.sleep(3)
+    # SENDER
+    import socket
+
+    group = '224.1.1.1'
+    port = 5004
+    # 2-hop restriction in network
+    ttl = 1
+
+    sock = socket.socket(socket.AF_INET,
+                         socket.SOCK_DGRAM,
+                         socket.IPPROTO_UDP)
+    sock.setsockopt(socket.IPPROTO_IP,
+                    socket.IP_MULTICAST_TTL,
+                    ttl)
+
+    sock.sendto(b'hello udp client', (group, port))
+
+    # # udpSendImage()
+    # # SENDER
+    # group = '224.1.1.1'
+    # port = 5004
+    # # 1-hop restriction in network
+    # ttl = 1
+    # sock = socket.socket(socket.AF_INET,
+    #                      socket.SOCK_DGRAM,
+    #                      socket.IPPROTO_UDP)
+    # sock.setsockopt(socket.IPPROTO_IP,
+    #                 socket.IP_MULTICAST_TTL,
+    #                 ttl)
+
+    # if channelRequestTime in AllChannels_playList["channel3"]["fast and furious 10"]:
+    #
+    #
+    #
+    #
+    #
+    # elif channelRequestTime in AllChannels_playList["channel3"]["venom"]:
